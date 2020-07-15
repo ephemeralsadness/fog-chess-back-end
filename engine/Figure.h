f887a544e1a320300b991365306a2824677bc5d3
ephemeralsad <sarancev.dev@gmail.com>
2020-07-15 12:06:06 +1000

    ColoredFigureHash был добавлен

--- engine/Figure.h
+++ engine/Figure.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 
 enum class Color {
     WHITE,
@@ -11,7 +12,6 @@ enum class Figure {
     PAWN,
     KNIGHT,
     BISHOP,
-    ROOK,
     QUEEN,
     KING
 };
@@ -22,4 +22,11 @@ struct ColoredFigure {
 
     ColoredFigure(Color color, Figure figure)
         : color(color), figure(figure) {}
+};
+
+struct ColoredFigureHash {
+    // При этом хеш-значения { WHITE, NOTHING } и { BLACK, NOTHING } равны.
+    size_t operator() (ColoredFigure f) const noexcept {
+        return static_cast<int>(f.figure) * (static_cast<int>(f.color) + 1);
+    }
 };
