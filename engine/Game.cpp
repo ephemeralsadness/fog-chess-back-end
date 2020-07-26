efa7025ba3270af0bf276a891f8256cd1218ab7f
ephemeralsad <sarancev.dev@gmail.com>
2020-07-27 03:01:48 +1000

    Реализация класса Game

--- engine/Game.cpp
+++ engine/Game.cpp
@@ -1,1 +1,17 @@
 #include "Game.h"
+
+Chessboard& Game::GetChessboard() {
+    return *chessboard;
+}
+
+bool Game::CheckPlayerWhites(unsigned int id) {
+    return id == player_whites;
+}
+
+bool Game::CheckPlayerBlacks(unsigned int id) {
+    return id == player_blacks;
+}
+
+GameStatus Game::GetStatus() {
+    return status;
+}
--- engine/Game.h
+++ engine/Game.h
@@ -4,9 +4,28 @@
 
 #include <memory>
 
+enum GameStatus {
+    NOT_STARTED,
+    ONGOING,
+    FINISHED
+};
+
+
 class Game {
 public:
+    Game(unsigned int player_whites, unsigned int player_blacks)
+        : chessboard(new Chessboard()),
+          player_whites(player_whites),
+          player_blacks(player_blacks),
+          status(GameStatus::NOT_STARTED) {}
 
+    Chessboard& GetChessboard();
+    bool CheckPlayerWhites(unsigned int id);
+    bool CheckPlayerBlacks(unsigned int id);
+    GameStatus GetStatus();
 private:
     std::unique_ptr<Chessboard> chessboard;
+    unsigned int player_whites;
+    unsigned int player_blacks;
+    GameStatus status;
 };
